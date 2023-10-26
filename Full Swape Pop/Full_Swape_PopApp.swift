99ea1e892dacebe87681eadf37374eb74248000d
sannma31 <kasaisyouun2004@gmail.com>
2023-10-26 23:35:19 +0900

    Initial Commit

---
+++ Full Swape Pop/Full_Swape_PopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Full_Swape_PopApp.swift
+//  Full Swape Pop
+//
+//  Created by 笠井翔雲 on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Full_Swape_PopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
